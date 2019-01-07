23047a6dd976569ad985484cef21a39880de08af
alisa-pogarskaya <alisapogarskaya@gmail.com>
2019-01-07 18:04:59 +0600

    Доработка табов + карусель
    Доработала табы, теперь в рабочем состоянии. Добавила карусель.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,73 @@ function view(n) {
     style = document.getElementById(n).style;
     style.display = (style.display == 'block') ? 'none' : 'block';
 }
+
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
+
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+
+document.getElementById('tabs').onclick= function (event) {
+    var target=event.target;
+    if (target.className=='tab') {
+       for (var i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+           }
+       }
+    }
+}
+
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('whiteborder');
+    }
+}
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
+
+function tabSwitch(tab) {
+    $('[id^=tab_').removeClass('active');
+    $('#tab_'+tab).addClass('active');        
+}
+
+  console.log(123);
+
+$(document).ready(function() {
+
+  console.log(123);
+
+    $('.owl-carousel').owlCarousel({
+        autoWidth: false,
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+});
